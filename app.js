d254d53099eef0db0ff747352a480ec6640d2667
DSK978 <69495698+DSK978@users.noreply.github.com>
2021-07-08 18:34:26 -0400

    update html

--- app.js
+++ app.js
@@ -4,18 +4,35 @@
 // const login = document.querySelector('#login')
 // const signin = document.querySelector('#signin')
 
-const username = document.querySelector('input[name="username"]')
-const password = document.querySelector('input[name="password"]')
-const form = document.querySelector('form')
+// username, password, button
+
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const form = document.querySelector('#btn');
 
 // event handlers
 
 // input validation requirements
-form.addEventListener('submit', listener:(Event: Event) => {
-    event.preventDefault();
-    if (isEmpty (username.value)) {
-        showError (username, 'Please enter your name');
-    } else {
-        login (username.value);
-    }
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+
 });
+
+function validate() {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue === '' || passwordValue === "") {
+        alert('Do not leave blank');
+        return false;
+    } else {
+        true;
+    }
+}
+
+// The password must contain at least one number
+// (?=.*[0-9])
+
+// The password must be eight characters or longer
+// (?=.{8,})
